3ed3222f9b26255dc50ab3a92560e27235b3243a
Diaz-adrianz <diazadrianz191205@gmail.com>
2023-04-23 23:26:22 +0700

    some litte update

--- server/database/db.js
+++ server/database/db.js
@@ -1,15 +1,10 @@
 import mongoose from 'mongoose';
 
-// const atlas =
-// 	'mongodb+srv://dazveloper:d4z-m0n9O4tL@cluster0.c76glns.mongodb.net/cobadulu?retryWrites=true&w=majority';
-const lokal = 'mongodb://127.0.0.1:27017/pagawei';
-const dockering = 'mongodb://mongodb:27017/newpagawei';
-
 mongoose.set('strictQuery', false);
 
 export const connectDB = async () => {
 	try {
-		const res = await mongoose.connect(lokal, {
+		const res = await mongoose.connect(process.env.MONGO_URI, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 		});
